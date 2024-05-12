6295c3bfc32f873b885407fb04c0336f08b03ef7
Ruaaa1983 <michaelgnu1983@outlook.com>
2024-05-12 20:09:22 +0800

    changerepo

--- .history/docs/.vitepress/theme/index_20240509154637.ts
+++
@@ -1,8 +0,0 @@
-import Theme from "vitepress/theme";
-import "./style/var.css";
-import Mycomponent from "./Layout.vue"
-
-
-export default {
-  ...Theme,
-};
--- .history/docs/.vitepress/theme/index_20240509182609.ts
+++
@@ -1,9 +0,0 @@
-import Theme from "vitepress/theme";
-import "./style/var.css";
-import Layout from './Layout/index.vue'
-
-
-export default {
-  ...Theme,
-
-};
